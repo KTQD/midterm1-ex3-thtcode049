86c54c56c63461d9b2017b40a948217911100029
thtcode049 <130653444+thtcode049@users.noreply.github.com>
2024-03-14 08:17:02 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
 
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+            StringBuilder content = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+            reader.close();
 
-        Bài làm viết tại đây
+            String replacedContent = content.toString().replaceAll("Nha Trang", "Vũng Tàu");
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+            writer.write(replacedContent);
+            writer.close();
 
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
     }
 }
